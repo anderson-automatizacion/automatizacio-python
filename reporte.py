c29cf1ea72528bff64d853c47cab4d84e87b53bc
Anderson <andersonz0112@hotmail.com>
2026-02-17 10:59:03 -0500

    Actualizacion automatizacion reportes

--- reporte.py
+++ reporte.py
@@ -1,21 +1,9 @@
-import pandas as pd
+import pandas as pd 
 import os
-
-# Crear carpeta si no existe
+archivo = input("Ingrese nombre del archivo Excel: ")
+df = pd.read_excel(archivo)
 os.makedirs("reporte_filtrado", exist_ok=True)
-
-# Leer archivo Excel
-df = pd.read_excel("datos.xlsx.xlsx")
-
-# Ver columnas reales
-print("Columnas encontradas:")
-print(df.columns)
-
-# Filtrar datos
-df_filtrado = df[df["VENTAS"] > 1000]
-
-# Guardar archivo
+df_filtrado = df[df["Ventas"] > 1000]
 ruta = "reporte_filtrado/reporte_filtrado.xlsx"
 df_filtrado.to_excel(ruta, index=False)
-
 print("Reporte creado correctamente")
